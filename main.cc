210ad95c2f3019d3acb7645365c75550fe0f3e11
ShariqAli-Dev <shariqali.dev@gmail.com>
2024-07-02 00:43:49 -0400

    brack placement

--- main.cc
+++ main.cc
@@ -36,7 +36,7 @@ static bool pause = false;
 
 static Player player = {0};
 static Ball ball = {0};
-static Brick brick[LINES_OF_BRICKS][BRICKS_PER_LINE] = {0};
+static Brick bricks[LINES_OF_BRICKS][BRICKS_PER_LINE] = {0};
 static Vector2 brick_size = {0};
 
 static void InitGame(void);
@@ -69,6 +69,23 @@ void InitGame(void)
     player.position = (Vector2){screen_width / 2, screen_height * 7 / 8};
     player.size = (Vector2){(float)GetScreenWidth() / 10.0f, 20.0f};
     player.life = PLAYER_MAX_LIFE;
+
+    // init the ball
+    ball.active = false;
+    ball.position = (Vector2){screen_width / 2, screen_height * 7 / 8};
+    ball.speed = {0, 0};
+    ball.radius = 7;
+
+    // init bricks
+    int initial_down_position = 50;
+    for (int b_row = 0; b_row < LINES_OF_BRICKS; b_row++)
+    {
+        for (int b_col = 0; b_col < BRICKS_PER_LINE; b_col++)
+        {
+            bricks[b_row][b_col].active = true;
+            bricks[b_row][b_col].position = (Vector2){b_col * brick_size.x + brick_size.x / 2, b_row * brick_size.y + initial_down_position};
+        }
+    }
 }
 
 void UpdateGame(void)
@@ -109,7 +126,34 @@ void DrawGame(void)
 
     if (!game_over)
     {
+        // player bar
         DrawRectangle(player.position.x - player.size.x / 2, player.position.y - player.size.y / 2, player.size.x, player.size.y, BLACK);
+
+        // player lives
+        for (int life = 0; life < player.life; life++)
+        {
+            DrawRectangle(20 + 40 * life, screen_height - 30, 35, 15, LIGHTGRAY);
+        }
+        // draw circle
+        DrawCircleV(ball.position, ball.radius, MAROON);
+
+        // draw bricks
+        for (int b_row = 0; b_row < LINES_OF_BRICKS; b_row++)
+        {
+            for (int b_col = 0; b_col < BRICKS_PER_LINE; b_col++)
+            {
+                if (bricks[b_row][b_col].active)
+                {
+
+                    Color b_color;
+                    if ((b_row + b_col) % 2 == 0)
+                        b_color = GRAY;
+                    else
+                        b_color = DARKGRAY;
+                    DrawRectangle(bricks[b_row][b_col].position.x - brick_size.x / 2, bricks[b_row][b_col].position.y - brick_size.y / 2, brick_size.x, brick_size.y, b_color);
+                }
+            }
+        }
 
         if (pause)
         {
